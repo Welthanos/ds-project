96384aabe3843c9e29ce78905d71ba9b8b9bc576
Welton <welthanos_dev@outlook.com>
2021-09-12 21:11:19 -0300

    BarChart integration

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -10,7 +10,6 @@ function Dashboard() {
             <NavBar />
             <div className="container">
                 <h1 className="text-primary py-3">DashBoard de Vendas</h1>
-
                 <div className="row px-3">
                     <div className="col-sm-6">
                         <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
@@ -21,7 +20,6 @@ function Dashboard() {
                         <DonutChart />
                     </div>
                 </div>
-
                 <div className="py-3">
                     <h2 className="text-primary">Todas Vendas</h2>
                 </div>
